aeca05abb29de8d546e0e9660f360de85c1d4c77
carlotroxler <carlo_troxler@hotmail.com>
2022-03-17 09:00:22 +0100

    Create randomizer function for Computer's choice.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+// computer's choice randomizer
+let choices = ['Rock', 'Paper', 'Scissors'];
+
+function computerPlay(){
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    console.log('The Computer has chosen: ' + computerChoice);
+    return computerChoice;
+
+};
+
